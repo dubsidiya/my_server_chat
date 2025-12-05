915841f310d6b91239c9aedd912f5cfb95013e1f
vladkharin <evapve@mail.ru>
2025-12-05 14:50:10 +0300

    111111

--- db.js
+++ db.js
@@ -1,9 +1,14 @@
 import pkg from 'pg';
 const { Pool } = pkg;
 
+import dotenv from 'dotenv';
+dotenv.config();
+
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: { rejectUnauthorized: false }
+  ssl: {
+    rejectUnauthorized: false, // <--- ОБЯЗАТЕЛЕН для Supabase
+  },
 });
 
 export default pool;
